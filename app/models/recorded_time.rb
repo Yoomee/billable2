7bb91f1d684777bcb8330d38d0e9cb328168d331
Joel Mayer <molemayer@gmail.com>
2014-05-28 11:05:40 +0100

    Add method to check if recorded_time is billable

--- app/models/recorded_time.rb
+++ app/models/recorded_time.rb
@@ -6,4 +6,25 @@ class RecordedTime < ActiveRecord::Base
   validates_associated :user
   validates :hours, :presence => true
   validates :date, :presence => true
+  
+  def billable?
+    budget = passed = 0
+    self.todo_list_item.todo_list.todo_list_items.each do |todo_list_item|
+      if todo_list_item == self.todo_list_item
+        todo_list_item.recorded_times.each do |recorded_time|
+          if passed == 0
+            budget += recorded_time.hours
+          end
+          if recorded_time == self
+            passed = 1
+          end
+        end
+      else
+        if passed == 0
+          budget += todo_list_item.recorded_times.sum(:hours)
+        end
+      end
+    end
+    budget <= self.todo_list_item.todo_list.budget
+  end
 end
