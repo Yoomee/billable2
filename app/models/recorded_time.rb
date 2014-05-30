a415fece2ebc5335e3f8d9a755b0ce2396df5849
Joel Mayer <molemayer@gmail.com>
2014-05-30 10:53:25 +0100

    Add temp method

--- app/models/recorded_time.rb
+++ app/models/recorded_time.rb
@@ -1,4 +1,6 @@
 class RecordedTime < ActiveRecord::Base
+  after_create :check_if_non_billable
+  
   belongs_to :todo_list_item
   belongs_to :user
   
@@ -26,5 +28,27 @@ class RecordedTime < ActiveRecord::Base
       end
     end
     budget <= self.todo_list_item.todo_list.budget
+  end
+  
+  def check_if_non_billable
+    budget = 0
+    self.todo_list_item.todo_list.todo_list_items.each do |todo_list_item|
+      if todo_list_item == self.todo_list_item
+        todo_list_item.recorded_times.each do |recorded_time|
+          if recorded_time == self
+            if budget > self.todo_list_item.todo_list.budget
+              self.non_billable_time = self.hours
+            elsif budget + self.hours > self.todo_list_item.todo_list.budget
+              self.non_billable_time = budget + self.hours - self.todo_list_item.todo_list.budget
+            end
+            save!
+          else
+            budget += recorded_time.hours
+          end
+        end
+      else
+        budget += todo_list_item.recorded_times.sum(:hours)
+      end
+    end
   end
 end
